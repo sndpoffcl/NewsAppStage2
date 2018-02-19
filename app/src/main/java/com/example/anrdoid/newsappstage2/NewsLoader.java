469b808022973e5986a666e7cd874fbf0682477a
Muneera_salah <muneera.salah@gmail.com>
2018-02-19 04:25:20 +0300

    post code

---
+++ app/src/main/java/com/example/anrdoid/newsappstage2/NewsLoader.java
@@ -0,0 +1,35 @@
+package com.example.anrdoid.newsappstage2;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by Labtop on 18/02/18.
+ */
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+   // private static String REQUEST_URL ="http://content.guardianapis.com/search?q=debates&api-key=test";
+    String mURL ;
+    public NewsLoader(Context context, String url) {
+        super(context);
+        this.mURL = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<News> loadInBackground() {
+        if (mURL == null) {
+            return null;
+        }
+
+        List<News> newsList = QueryUtils.fetchNewsData(mURL);
+        return newsList;
+    }
+}
